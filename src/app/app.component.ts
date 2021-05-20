ff2be32d7c41e484e4671a558a43148416fddb60
aptopcoder <aptopcoder@gmail.com>
2021-05-20 23:37:30 +0530

    Integrated Monaco Editor v9 and Bootstrap v3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   mathml = '';
+  editorOptions = { language: 'html' };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
 
 import { AppComponent } from './app.component';
 import { MathModule } from './math/math.module';
 
+const defaultEditorConfig: NgxMonacoEditorConfig = {
+  defaultOptions: {
+    theme: 'vs-dark',
+    automaticLayout: true,
+    minimap: {
+      enabled: false
+    },
+    scrollBeyondLastLine: false,
+    scrollbar: {
+      vertical: "hidden",
+      horizontal: "hidden"
+    }
+  }
+};
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MathModule ],
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    MathModule,
+    MonacoEditorModule.forRoot(defaultEditorConfig)
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
